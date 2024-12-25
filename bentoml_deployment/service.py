3fde61c53883103b3544aeb92d66dd62d1aa9cda
rubanzasilva <rubanza@rubanzasilver.xyz>
2024-12-25 12:01:03 +0300

    new changes

--- bentoml_deployment/service.py
+++ bentoml_deployment/service.py
@@ -10,8 +10,15 @@ fastbook.setup_book()
 from fastbook import *
 from fastai.tabular.all import *
 
+from typing import Annotated  # Python 3.9 or above
+from typing_extensions import Annotated  # Older than 3.9
+import pandas as pd
+from bentoml.validators import DataframeSchema
+import bentoml
+
+
 @bentoml.service(
-    resources={"cpu": "2"},
+    resources={"cpu": "8"},
     traffic={"timeout": 10},
 )
 
@@ -22,22 +29,19 @@ class MentalHealthClassifier:
     def __init__(self):
         self.model = bentoml.xgboost.load_model(self.bento_model)
 
-        #check resource availability
-
-        if os.getenv("CUDA_VISIBLE_DEVICES") not in (None, "", "-1"):
-            self.model.set_param({"predictor": "gpu_predictor", "gpu_id": 0})  # type: ignore (incomplete XGBoost types)
-        else:
-            nthreads = os.getenv("OMP_NUM_THREADS")
-            if nthreads:
-                nthreads = max(int(nthreads), 1)
-            else:
-                nthreads = 1
-            self.model.set_param(
-                {"predictor": "cpu_predictor", "nthread": nthreads}
-            )
-
        
     @bentoml.api
-    def predict(self,data):
-        return self.predict(tensor(data))
+    def predict( self,
+        input: Annotated[pd.DataFrame, DataframeSchema(orient="records", columns=[{
+    "id": 140700,"Name": "Shivam","Gender": "Male","Age": 53,
+    "City":"Visakhapatnam","Working Professional or Student": "Working Professional","Profession": "Judge", "Academic Pressure":5,
+    "Work Pressure": 4,"CGPA": 6.84,"Study Satisfaction": 1,"Job Satisfaction": 3,
+    "Sleep Duration": "Less than 5 hours","Dietary Habits": "Moderate","Degree": "B.Ed","Have you ever had suicidal thoughts ?": "No",
+    "Work/Study Hours": 6,"Financial Stress": 4,"Family History of Mental Illness": "Yes"
+}
+])]
+    ) -> int:
 
+        return self.model.predict(input)
+
+        #return self.predict(data)
